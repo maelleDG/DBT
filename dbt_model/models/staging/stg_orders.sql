ab14d6af5441e530c675633d12c9126d265f415b
nom_d_utilisateur_githubmaelleDG <gaudin.maelle79@gmail.com>
2025-06-30 16:32:36 +0200

    quête 1.3 dbt models

---
+++ dbt_model/models/staging/stg_orders.sql
@@ -0,0 +1,14 @@
+with source as (
+    select * from {{ source('my_dbt_db', 'raw_orders') }}
+),
+
+renamed as (
+    select
+        id as orders_id, 
+        customer as customer_id, 
+        ordered_at as order_date, 
+        store_id
+    from source
+)
+
+select * from renamed
